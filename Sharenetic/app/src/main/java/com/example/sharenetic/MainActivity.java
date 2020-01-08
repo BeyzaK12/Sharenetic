26fdf2a8540e8f86a79571fd37174a376ef6a9f7
beyzak12 <beyzakurt1998@hotmail.com>
2020-01-09 00:21:59 +0300

    cont

--- Sharenetic/app/src/main/java/com/example/sharenetic/MainActivity.java
+++ Sharenetic/app/src/main/java/com/example/sharenetic/MainActivity.java
@@ -1,29 +1,32 @@
 package com.example.sharenetic;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.ImageView;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
 
     private ImageButton sharePostBtn;
-    private ImageButton commentsBtn;
     private Button calendarBtn;
     private ImageButton logout;
+
+    private homeFragment homeFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        homeFragment = new homeFragment();
+        replaceFragment(homeFragment);
+
 
         sharePostBtn = (ImageButton) findViewById(R.id.addPostBtn);
         sharePostBtn.setOnClickListener(new View.OnClickListener() {
@@ -35,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        /*
         commentsBtn = (ImageButton) findViewById(R.id.commentsBtn);
         commentsBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,6 +48,7 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         });
+        */
 
         calendarBtn = (Button) findViewById(R.id.calendarBtn);
         calendarBtn.setOnClickListener(new View.OnClickListener() {
@@ -71,5 +76,12 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.main_container, fragment);
+        fragmentTransaction.commit();
+
     }
 }
